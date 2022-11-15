98076715f250c8091791259256762b00356e99e4
Pavel Rogovoy <p.rogovoy@gmail.com>
2022-11-15 20:53:20 +0200

    asas

--- dist/index.js
+++ dist/index.js
@@ -9728,8 +9728,9 @@ const main = async() => {
     await execute(`pip install vmn`);
     await execute(`pwd`);
     await execute(`ls -la ./`);
-    await execute(`ls -la ../`);
-    //await execute(`vmn init-app ${app_name}`);
+    await execute(`ls -la .vmn/${app_name}/`);
+    await execute(`git status`);
+    await execute(`vmn init-app ${app_name}`);
 
     err = await execute(`vmn --debug stamp -r ${release_mode} ${app_name}`);
     core.info(`stdout: ${err}`);
