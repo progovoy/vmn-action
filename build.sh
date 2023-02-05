3652764640e5e541bb8980cfba8580dfb11c6d07
Yuval Aharoni <yuvalaharoni585@gmail.com>
2023-02-05 19:19:14 +0200

    Fix 0.1.14

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@ npm i -g @vercel/ncc
 npm i 
 ncc build index.js -o dist
 
-git add -u && git commit -m "make build" && git push
-
-vmn stamp -r patch vmna
+git add -u && git commit -m "make build" && git push && vmn stamp -r patch vmna
 
 
