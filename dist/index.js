c69beddd7401b05b50b89c13f22e8bbb90de4807
Yuval Aharoni <yuvalaharoni585@gmail.com>
2023-03-30 16:39:37 +0300

    make build

--- dist/index.js
+++ dist/index.js
@@ -13939,7 +13939,7 @@ const main = async () => {
     // }   
 
     try{
-        await execute(`pip install vmn`);
+        await execute(`pip install --pre -U vmn`);
     } catch (e) {
         await fail(`Error executing pip install ${e}`);
     }
